71184e896d6d27c42e5300625b522e017df7bb2a
FabianAdrian <contact@fabianadrian.fi>
2024-08-19 20:29:24 +0300

    simplify code

--- src/main/java/fi/fabianadrian/operatorlevel/OperatorLevel.java
+++ src/main/java/fi/fabianadrian/operatorlevel/OperatorLevel.java
@@ -1,12 +1,26 @@
 package fi.fabianadrian.operatorlevel;
 
-import fi.fabianadrian.operatorlevel.listener.PlayerJoinListener;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class OperatorLevel extends JavaPlugin {
-
+public final class OperatorLevel extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
-		getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+
+		for (int i = 4; i > 0; i--) {
+			if (player.hasPermission("operatorlevel." + i)) {
+				player.sendOpLevel((byte) i);
+				break;
+			}
+		}
 	}
 }
