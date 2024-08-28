d457d3a93cdc6b70900cb460a7e1752db0dcc64f
FabianAdrian <contact@fabianadrian.fi>
2024-08-28 23:20:33 +0300

    only initialize LuckpermsListener if LuckPerms is present

--- src/main/java/fi/fabianadrian/operatorlevel/OperatorLevel.java
+++ src/main/java/fi/fabianadrian/operatorlevel/OperatorLevel.java
@@ -3,6 +3,7 @@ package fi.fabianadrian.operatorlevel;
 import fi.fabianadrian.operatorlevel.listener.LuckpermsListener;
 import fi.fabianadrian.operatorlevel.listener.PlayerListener;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OperatorLevel extends JavaPlugin {
@@ -22,7 +23,11 @@ public final class OperatorLevel extends JavaPlugin {
 	}
 
 	private void registerListeners() {
-		getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
-		new LuckpermsListener(this);
+		PluginManager manager = getServer().getPluginManager();
+		manager.registerEvents(new PlayerListener(this), this);
+
+		if (manager.isPluginEnabled("LuckPerms")) {
+			new LuckpermsListener(this);
+		}
 	}
 }
