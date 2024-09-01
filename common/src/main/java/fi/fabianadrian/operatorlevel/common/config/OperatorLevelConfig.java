34f9f76713126ca167f75d19a8010d85c07a6512
FabianAdrian <contact@fabianadrian.fi>
2024-09-01 18:33:57 +0300

    suppress config warnings

--- common/src/main/java/fi/fabianadrian/operatorlevel/common/config/OperatorLevelConfig.java
+++ common/src/main/java/fi/fabianadrian/operatorlevel/common/config/OperatorLevelConfig.java
@@ -3,6 +3,7 @@ package fi.fabianadrian.operatorlevel.common.config;
 import space.arim.dazzleconf.annote.ConfComments;
 import space.arim.dazzleconf.annote.ConfDefault;
 
+@SuppressWarnings("BooleanMethodIsAlwaysInverted")
 public interface OperatorLevelConfig {
 	@ConfComments("Use LuckPerms' meta system instead of permissions to define levels.")
 	@ConfDefault.DefaultBoolean(false)
