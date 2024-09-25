14498d6225b1c8b592bbe63e0000c5635f77cc78
Fabian Mäkilä <contact@fabianadrian.fi>
2024-09-25 19:10:27 +0300

    update shadow

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies() {
 	implementation("com.diffplug.spotless:spotless-plugin-gradle:7.0.0.BETA2")
-	implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.0")
+	implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.2")
 }
