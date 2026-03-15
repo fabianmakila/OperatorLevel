4c351239ebf692fe888063d490d22298a18bc881
Fabian Mäkilä <contact@fabianadrian.fi>
2026-03-15 14:47:01 +0200

    bump dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	`kotlin-dsl`
 }
 
-dependencies() {
-	implementation("com.diffplug.spotless:spotless-plugin-gradle:8.1.0")
-	implementation("com.gradleup.shadow:shadow-gradle-plugin:9.2.2")
+dependencies {
+	implementation("com.diffplug.spotless:spotless-plugin-gradle:8.3.0")
+	implementation("com.gradleup.shadow:shadow-gradle-plugin:9.4.0")
 }
