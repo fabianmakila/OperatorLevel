c3afbaf6ec8faaa9fee9585c3509ddb6f677b5c5
Fabian Mäkilä <contact@fabianadrian.fi>
2026-05-22 15:22:47 +0300

    remove probably unnecessary null check

--- common/src/main/java/fi/fabianadrian/operatorlevel/common/packet/PacketSender.java
+++ common/src/main/java/fi/fabianadrian/operatorlevel/common/packet/PacketSender.java
@@ -7,10 +7,6 @@ import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEn
 public final class PacketSender {
 	public void sendPacket(Object player, int level) {
 		User user = PacketEvents.getAPI().getPlayerManager().getUser(player);
-		if (user == null) {
-			return;
-		}
-
 		WrapperPlayServerEntityStatus packet = new WrapperPlayServerEntityStatus(user.getEntityId(), 24 + level);
 		user.sendPacketSilently(packet);
 	}
